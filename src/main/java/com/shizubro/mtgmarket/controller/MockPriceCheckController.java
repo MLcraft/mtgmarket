35a3f3143c177ff60c09511a8b0bce074f62b8cd
MLcraft <michelleliu.ca@gmail.com>
2024-09-26 21:27:39 -0700

    mocks

--- src/main/java/com/shizubro/mtgmarket/controller/MockPriceCheckController.java
+++ src/main/java/com/shizubro/mtgmarket/controller/MockPriceCheckController.java
@@ -2,27 +2,31 @@ package com.shizubro.mtgmarket.controller;
 
 import com.shizubro.mtgmarket.model.Card;
 import com.shizubro.mtgmarket.model.Listing;
-import com.shizubro.mtgmarket.service.PriceSearchService;
+import com.shizubro.mtgmarket.service.MockPriceSearchService;
 import com.shizubro.mtgmarket.service.ScryfallService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Set;
 
 @RestController
-@RequestMapping(path = "/")
-public class PriceCheckController {
-    PriceSearchService priceSearchService;
+@RequestMapping(path = "/mock/")
+public class MockPriceCheckController {
+    MockPriceSearchService priceSearchService;
     ScryfallService scryfallService;
 
     @Autowired
-    public PriceCheckController(PriceSearchService priceSearchServiceImpl,
-                                ScryfallService scryfallService) {
-        this.priceSearchService = priceSearchServiceImpl;
+    public MockPriceCheckController(MockPriceSearchService mockPriceSearchService,
+                                    ScryfallService scryfallService) {
+        this.priceSearchService = mockPriceSearchService;
         this.scryfallService = scryfallService;
     }
+
     // search for all prices for cardName
     @GetMapping("/search/{cardName}")
     public Set<Listing> getListingsByName(@PathVariable String cardName) {
--- src/main/java/com/shizubro/mtgmarket/service/MockPriceSearchService.java
+++ src/main/java/com/shizubro/mtgmarket/service/MockPriceSearchService.java
@@ -1,41 +1,53 @@
 package com.shizubro.mtgmarket.service;
 
+import com.shizubro.mtgmarket.model.Card;
 import com.shizubro.mtgmarket.model.Listing;
 import com.shizubro.mtgmarket.enums.CARDCONDITION;
 import com.shizubro.mtgmarket.enums.CARDLANG;
 import com.shizubro.mtgmarket.enums.CARDSHOP;
+import com.shizubro.mtgmarket.repository.CardRepository;
+import com.shizubro.mtgmarket.repository.ListingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Component("mockPriceSearchService")
 public class MockPriceSearchService {
+    @Autowired
+    ListingRepository listingRepository;
+
+    @Autowired
+    CardRepository cardRepository;
+
     public Set<Listing> getCardPriceByFilters(String cardName) {
         Set<Listing> resultSet = new HashSet<Listing>();
-//        Listing mockListing1 = Listing.builder()
-//                .source(CARDSHOP.SERRA)
-//                .lang(CARDLANG.EN)
-//                .isFoil(false)
-//                .listingUrl("listingUrl1")
-//                .price(BigInteger.valueOf(100))
-//                .condition(CARDCONDITION.SP)
-//                .cardImageUrl("https://cards.scryfall.io/png/front/f/5/f5a65d3b-83e7-4f32-89b3-d152e66f1868.png?1568005526")
-//                .build();
-//
-//        Listing mockListing2 = Listing.builder()
-//                .source(CARDSHOP.HARERUYA)
-//                .lang(CARDLANG.JP)
-//                .isFoil(true)
-//                .listingUrl("listingUrl2")
-//                .price(BigInteger.valueOf(123100))
-//                .condition(CARDCONDITION.NM)
-//                .cardImageUrl("https://cards.scryfall.io/png/front/9/6/96e5358a-23b6-4dac-893e-48f4481609ab.png?1696636954")
-//                .build();
-//
-//        resultSet.add(mockListing1);
-//        resultSet.add(mockListing2);
+        Card testCard = new Card();
+        UUID cardOracleId = UUID.randomUUID();
+        testCard.setOracleId(cardOracleId);
+        testCard.setCardName("Test Card Name");
+        this.cardRepository.save(testCard);
+
+        Listing testListing = new Listing();
+        testListing.setCard(testCard);
+        testListing.setId(1l);
+        testListing.setSource(CARDSHOP.SERRA);
+        testListing.setLang(CARDLANG.EN);
+        testListing.setSetCode("TSC");
+        testListing.setCardNumber("001A");
+        testListing.setFoil(false);
+        testListing.setListingUrl("listing.url");
+        testListing.setPrice(BigInteger.valueOf(10000));
+        testListing.setCondition(CARDCONDITION.SP);
+        testListing.setCardImageUrl("cardimage.url");
+        testListing.setUpdatedAt(Instant.parse("2024-11-30T18:35:24.00Z"));
+        testListing.setCreatedAt(Instant.parse("2024-09-23T12:13:42.00Z"));
+
+        this.listingRepository.save(testListing);
         return resultSet;
     }
 }
